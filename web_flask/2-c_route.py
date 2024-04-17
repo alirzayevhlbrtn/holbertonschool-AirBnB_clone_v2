3d4f21c2274a04f9c7b00347d6aa130912efd5a2
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-17 15:42:55 +0400

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+task 0
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """default page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """say hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """display text"""
+    txt = txt.replace("_", " ")
+    return f"C {txt}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
