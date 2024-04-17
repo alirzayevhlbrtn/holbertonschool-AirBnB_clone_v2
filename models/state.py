5e6cc2ea9dfd11c011cd1e960080f3b67806adb0
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-17 17:41:29 +0400

    7

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from models.city import City
 from os import getenv
 
 
