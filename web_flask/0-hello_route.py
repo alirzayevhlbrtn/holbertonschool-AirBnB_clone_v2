5c47e0e77a9381f6ab2bf044a0533af8e11f9547
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-17 15:05:54 +0400

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from flask import Flask
 """
 task 0
 """
-
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -14,5 +13,5 @@ def hello_world():
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
