15211ee311e5b9cbdc33463d11c55479616ed06a
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-21 20:14:27 +0400

    9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def display_cities_by_states():
-    states = sorted(storage.all("State").values(), key=lambda s: s.name)
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
     for state in states:
         state.cities = sorted(state.cities, key=lambda c: c.name)
     return render_template("8-cities_by_states.html", states=states)
